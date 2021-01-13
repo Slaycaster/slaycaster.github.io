f7e01ccb93f4366dacf7f9dea535d7277dc1107e
Denimar <fdenimar@hotmail.com>
2021-01-13 11:36:30 +0800

    Initial commit

---
+++ precache-manifest.45c3580d687d851ab5a97d4a89be34a2.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f933fb58f290d75ff4083603f981f20b",
+    "url": "/f14cb0b69c1d17543eec.worker.js"
+  },
+  {
+    "revision": "69cd844e5198ac97a0aad1207ced67af",
+    "url": "/f14cb0b69c1d17543eec.worker.js.LICENSE.txt"
+  },
+  {
+    "revision": "328a0874e0e5c40f1e19edcf3c1fdde2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a982934f8126a2638961",
+    "url": "/static/css/main.8490e643.chunk.css"
+  },
+  {
+    "revision": "d2b11456f3929ef6d4c4",
+    "url": "/static/js/2.f76569e4.chunk.js"
+  },
+  {
+    "revision": "dc0aa46f0a0a825c28d8a42e66e39698",
+    "url": "/static/js/2.f76569e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a982934f8126a2638961",
+    "url": "/static/js/main.7d9d49b3.chunk.js"
+  },
+  {
+    "revision": "8495a45be39eae487cea",
+    "url": "/static/js/runtime-main.e7db1af1.js"
+  },
+  {
+    "revision": "62cd93bfe63d7ab4f1cc8b7a2a93374a",
+    "url": "/static/media/circle-32.62cd93bf.png"
+  },
+  {
+    "revision": "67b2ae58cd6d5e97bcdc00bcdbf40392",
+    "url": "/static/media/measure-32.67b2ae58.png"
+  },
+  {
+    "revision": "465bcdec8b9d3b76b808449b68b1eb30",
+    "url": "/static/media/polygon-32.465bcdec.png"
+  },
+  {
+    "revision": "33dff5712064b8291a103de69e8de617",
+    "url": "/static/media/rectangle-32.33dff571.png"
+  },
+  {
+    "revision": "136c70365095ba7939a6139a84c63f21",
+    "url": "/static/media/sample.136c7036.pdf"
+  }
+]);
